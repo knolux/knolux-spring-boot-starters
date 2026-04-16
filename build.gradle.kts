36971d78076ef5403aec7681a47b99215b06b843
WayneChen <wenwen357951@gmail.com>
2026-04-16 22:28:41 +0800

    refactor: update GitHub Packages URLs to knolux-spring-boot-starters

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ subprojects {
                 pom {
                     name.set(project.name)
                     description.set(project.description ?: "")
-                    url.set("https://github.com/knolux/knolux-starter")
+                    url.set("https://github.com/knolux/knolux-spring-boot-starters")
                     licenses {
                         license {
                             name.set("MIT License")
@@ -58,7 +58,7 @@ subprojects {
         repositories {
             maven {
                 name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/knolux/knolux-starter")
+                url = uri("https://maven.pkg.github.com/knolux/knolux-spring-boot-starters")
                 credentials {
                     username = System.getenv("GITHUB_ACTOR") ?: ""
                     password = System.getenv("GITHUB_TOKEN") ?: ""
