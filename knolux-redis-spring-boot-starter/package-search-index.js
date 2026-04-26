121f4eb0f8b675b2aa4ffbdfebdfe75cfffc283a
wenwen357951 <wenwen357951@gmail.com>
2026-04-26 13:55:14 +0000

    Deploying to gh-pages from @ knolux/knolux-spring-boot-starters@45140079e5a54124eb50f977c26ee1ad81a81308 🚀

--- knolux-redis-spring-boot-starter/package-search-index.js
+++ knolux-redis-spring-boot-starter/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.knolux.redis"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.knolux.redis"},{"l":"com.knolux.redis.connection"}];updateSearchResults();
